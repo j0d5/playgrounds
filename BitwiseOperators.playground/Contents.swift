ebf7c2b36641c05b8fd8fdafa938b763eee2b12b
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2017-11-03 14:41:23 +0100

    Initial commit

---
+++ BitwiseOperators.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+var test = 0xF
+
+
+var bit = 0b0110
+let negatedBit = test & ~bit
+
+print(String(test, radix: 2))
+print("0\(String(bit, radix: 2))")
+print(String(negatedBit, radix: 2))
+
+
+
+bit = 0b0110
+let xorBit = test ^ bit
+
+print(String(test, radix: 2))
+print("0\(String(bit, radix: 2))")
+print(String(xorBit, radix: 2))
+
---
+++ TypeCasting.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var integer: Int = -1
+var nsuInt: UInt = UInt(integer)
+
+print(nsuInt)
+
