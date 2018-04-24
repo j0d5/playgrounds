77781e49e8e1706e07f4a80b75c73680992cbd6a
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2018-04-24 19:38:41 +0200

    Add for playground

---
+++ forSyntax.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+let testArray = 0...10
+//print(array.enumerated())
+
+for item in testArray {
+    print(item)
+}
+
+for count in 0...testArray.count {
+    print(count)
+}
+
+for count in 0...testArray.count - 1 {
+    print(count)
+}
+
+for count in 0..<testArray.count {
+    print(count)
+}
+
+// for count in 0...length - 1
+// for count in 0..<length
